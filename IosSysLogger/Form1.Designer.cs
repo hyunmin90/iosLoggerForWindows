decbdba335d6799472468b37c0c36411452b8639
Hyounmin Steve Wang <hyunmin90@gmail.com>
2015-07-09 18:02:37 +0900

    added search bar and table view

--- IosSysLogger/Form1.Designer.cs
+++ IosSysLogger/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace IosSysLogger
 {
-    partial class Form1
+    partial class iosSyslogger
     {
         /// <summary>
         /// 필수 디자이너 변수입니다.
@@ -28,38 +28,155 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.searchBtn = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.date = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.device = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Process = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LogLevel = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Log = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.clearSearchBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // textBox1
+            // textBox2
             // 
-            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Location = new System.Drawing.Point(12, 234);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(0, 0, 0, 0);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBox1.Size = new System.Drawing.Size(1017, 258);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox2.Location = new System.Drawing.Point(740, 15);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(200, 21);
+            this.textBox2.TabIndex = 1;
             // 
-            // Form1
+            // searchBtn
+            // 
+            this.searchBtn.Location = new System.Drawing.Point(946, 15);
+            this.searchBtn.Name = "searchBtn";
+            this.searchBtn.Size = new System.Drawing.Size(75, 23);
+            this.searchBtn.TabIndex = 2;
+            this.searchBtn.Text = "search";
+            this.searchBtn.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowDrop = true;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.date,
+            this.device,
+            this.Process,
+            this.LogLevel,
+            this.Log});
+            this.dataGridView1.Location = new System.Drawing.Point(3, 266);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(1024, 202);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_1);
+            // 
+            // date
+            // 
+            this.date.FillWeight = 28.03262F;
+            this.date.HeaderText = "Date";
+            this.date.Name = "date";
+            // 
+            // device
+            // 
+            this.device.FillWeight = 71.15302F;
+            this.device.HeaderText = "Device";
+            this.device.Name = "device";
+            // 
+            // Process
+            // 
+            this.Process.FillWeight = 118.5104F;
+            this.Process.HeaderText = "Process";
+            this.Process.Name = "Process";
+            // 
+            // LogLevel
+            // 
+            this.LogLevel.FillWeight = 160.6675F;
+            this.LogLevel.HeaderText = "LogLevel";
+            this.LogLevel.Name = "LogLevel";
+            // 
+            // Log
+            // 
+            this.Log.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Log.FillWeight = 121.6365F;
+            this.Log.HeaderText = "Log";
+            this.Log.Name = "Log";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(1017, 266);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(10, 10);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "All",
+            "Debug",
+            "Info",
+            "Notice",
+            "Warning",
+            "Error",
+            "Critical",
+            "Alert",
+            "Emergency"});
+            this.checkedListBox1.Location = new System.Drawing.Point(502, 15);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(130, 180);
+            this.checkedListBox1.TabIndex = 6;
+            // 
+            // clearSearchBtn
+            // 
+            this.clearSearchBtn.Location = new System.Drawing.Point(946, 45);
+            this.clearSearchBtn.Name = "clearSearchBtn";
+            this.clearSearchBtn.Size = new System.Drawing.Size(75, 36);
+            this.clearSearchBtn.TabIndex = 7;
+            this.clearSearchBtn.Text = "clear search";
+            this.clearSearchBtn.UseVisualStyleBackColor = true;
+            // 
+            // iosSyslogger
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1033, 496);
-            this.Controls.Add(this.textBox1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.clearSearchBtn);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.searchBtn);
+            this.Controls.Add(this.textBox2);
+            this.Name = "iosSyslogger";
+            this.Text = "iosSysLogger";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button searchBtn;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn date;
+        private System.Windows.Forms.DataGridViewTextBoxColumn device;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Process;
+        private System.Windows.Forms.DataGridViewTextBoxColumn LogLevel;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Log;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button clearSearchBtn;
     }
 }
 
--- IosSysLogger/Form1.cs
+++ IosSysLogger/Form1.cs
@@ -11,69 +11,154 @@ using System.IO;
 
 namespace IosSysLogger
 {
-    public partial class Form1 : Form
+    public partial class iosSyslogger : Form
     {
-        public Form1()
+        int start = 0;
+        int indexOfSearchText = 0;
+        int rowNumber = 0;
+        bool firstRowf = false;
+        
+        public iosSyslogger()
         {
             
             InitializeComponent();
             
             string currentPath = System.Environment.CurrentDirectory;
-
-
-           /* FileSystemWatcher watcher = new FileSystemWatcher();
-            watcher.Path = currentPath;
-            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
-                                   | NotifyFilters.FileName | NotifyFilters.DirectoryName;
-            watcher.Filter = "*.*";
-            watcher.Changed += new FileSystemEventHandler(OnChanged);
-            watcher.EnableRaisingEvents = true;
-            */
-
-
-
-            /*string[] fileContents;
-
-            try
-            {
-                fileContents = File.ReadAllLines(currentPath + @"\syslog.txt");
-
-                foreach (string line in fileContents)
-                {
-                    textBox1.AppendText(line+"\n");
-                }
-            }
-            catch (FileNotFoundException ex)
-            {
-                throw ex;
-            }
-            catch (Exception e)
-            {
-                throw e;
-            }
-            Console.ReadLine();*/
+            this.searchBtn.Click += new System.EventHandler(this.searchBtn_Click);
+            this.clearSearchBtn.Click += new System.EventHandler(this.clearSearchBtn_Click);
 
         }
 
         public string TextBoxText
         {
-            get { return textBox1.Text; }
-            set { textBox1.AppendText(value+'\n'); }
-        }
+            get { return null; }
+            set
+            {
+                int index = 0;
+                bool highlightNew = false;
+                string[] month = { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
+                if (month.Any(e => value.StartsWith(e))) //Start with one of the month code
+                {
+                   
+                    dataGridView1.Rows.Add(); 
+                    //richTextBox1.AppendText(value + Environment.NewLine);
 
-        /* private void OnChanged(object source, FileSystemEventArgs e)
-         {
-             textBox1.AppendText("hello ah");
-         }
-         */
+                    string[] words = value.Split(' ');
+                    foreach (string word in words)
+                    {
+                        if (textBox2.Text!=""&& word.Contains(textBox2.Text))
+                        {
+                            highlightNew = true;
+                        }
+                        if (month.Any(e => word.StartsWith(e)))
+                        {
+                            firstRowf = true;
+                        }
+                        if (firstRowf == true && index<4) //Date
+                            dataGridView1.Rows[rowNumber].Cells[0].Value += word+" ";
+                        else if(firstRowf == true &&index==4) //Device
+                            dataGridView1.Rows[rowNumber].Cells[1].Value += word + " ";
+                        else if (firstRowf == true && index == 5) //Process
+                            dataGridView1.Rows[rowNumber].Cells[2].Value += word + " ";
+                        else if (firstRowf == true && (index == 6||word.Contains(">:"))) //LogLevel
+                            dataGridView1.Rows[rowNumber].Cells[3].Value += word + " ";
+                        else if (firstRowf == true && index > 6)//Log
+                            dataGridView1.Rows[rowNumber].Cells[4].Value += word + " ";
+                        index++;
+                    }
+                    if (highlightNew == true)
+                    {
+                        dataGridView1.Rows[rowNumber].Selected = true;
+                        dataGridView1.Rows[rowNumber].Visible = true;
+                    }
+                    else if (highlightNew == false && textBox2.Text != "")
+                    {
+                        dataGridView1.Rows[rowNumber].Visible = false;
+                        dataGridView1.Rows[rowNumber].Selected = false;
+                    }
+                    rowNumber++;
+                    firstRowf = false;
+                    dataGridView1.FirstDisplayedScrollingRowIndex = dataGridView1.RowCount - 1;
+
+                    //dataGridView1.Rows[0].Cells[4].Value = value;
+                }
+            }
+
+        }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
         
+ 
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void searchBtn_Click(object sender, EventArgs e)
+        {
+            string search = textBox2.Text;
+            highLight(search);   
+        }
+        private void clearSearchBtn_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+            clearHighlight();   
+        }
+        private void clearHighlight()
+        {
+            for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+            {
+                if (dataGridView1.Rows[i].Cells[3] == null)
+                { return; }
+                
+                    dataGridView1.Rows[i].Selected = false;
+                    dataGridView1.Rows[i].Visible = true;
+                
+
+                
+            }
+        }
+        private void highLight(string term)
+        {
+            
+            if (term != null)
+            {
+                for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+                {
+                    if (dataGridView1.Rows[i].Cells[3] == null)
+                    { return; }
+                    else if (dataGridView1.Rows[i].Cells[0].Value.ToString().Contains(term) || dataGridView1.Rows[i].Cells[1].Value.ToString().Contains(term) || dataGridView1.Rows[i].Cells[2].Value.ToString().Contains(term) || dataGridView1.Rows[i].Cells[3].Value.ToString().Contains(term) || dataGridView1.Rows[i].Cells[4].Value.ToString().Contains(term))
+                    {
+                        dataGridView1.Rows[i].Selected = true;
+                        dataGridView1.Rows[i].Visible = true;
+                    }
+                    else
+                    {
+                        dataGridView1.Rows[i].Visible = false;
+                        dataGridView1.Rows[i].Selected = false;
+                    }
+
+
+                }
+            }
+
+
+        }
+
+        
+
+        // Reset the richtextbox when user changes the search string
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            start = 0;
+            indexOfSearchText = 0;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
         {
 
         }
